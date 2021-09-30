ee4db84ef7064115343e96bd283abcc6a023dad7
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-09-30 15:12:35 +0100

    adding in the BlockService, FlowService and QueryEventsService

--- FlowEventMonitorLambda/package/QueryEventsService/index.js
+++ FlowEventMonitorLambda/package/QueryEventsService/index.js
@@ -1,44 +1,40 @@
-module.exports.getListingAvailableEvents = async () => {
-  // Get the most recent sealed block
-  // TODO: Uncomment
-  // const block = await fcl.send([fcl.getBlock(true)]);
-  // decoded = await fcl.decode(block);
+const { getBlockCursorForEvent } = require("../BlockCursorTableService");
+const { getCurrentBlockHeight, searchBlockRange } = require("../FlowService");
 
-  // Get the current block height
-  // TODO: Uncomment
-  // startingBlockHeight = decoded.height
-  // console.log(`height: ${startingBlockHeight}`)
+// @params: [String]
+const getEvents = async (eventsArray) => {
+  const eventName = "ListingAvailable";
+  const contractAddress = "0x29e893174dd9b963";
+  const contractName = "DappyContract";
 
-  // Get the block cursor
-  // If no block cursor, set it to the current block height - 249; that's the largest spread you can do.
-  // Query dDB to get bH.
-  // TODO: Update fromBlock with cursor
+  // const TEST_EVENTS_ARRAY = ["aMadeUpEvent", "aNonExistantEvent"];
+  const TEST_EVENTS_ARRAY = [
+    `A.${"TokensWithdrawn"}.${"FUSD"}.${"e223d8a629e49c68"}`,
+    `A.${"ListingAvailable"}.${"DappyContract"}.${"0x29e893174dd9b963"}`,
+  ];
 
-  // TODO: Uncomment
-  // const fromBlock = startingBlockHeight - 249
-  // const toBlock = startingBlockHeight
+  const currentBlockHeight = await getCurrentBlockHeight();
+  console.log(`!!!CurrentBlockHeight: ${JSON.stringify(currentBlockHeight)}`);
+
+  // We can use the eventName as an ID as it should be unique
+  // Returns { eventName, blockCursor }
+  const cursors = await Promise.all(
+    TEST_EVENTS_ARRAY.map(async (event) => {
+      return await getBlockCursorForEvent(event, currentBlockHeight);
+    })
+  );
+  console.log(`!!!cursors: ${JSON.stringify(cursors)}`);
+
+  const eventObjectsArray = await Promise.all(
+    cursors.map(async (cursor) => {
+      return await searchBlockRange(currentBlockHeight, cursor);
+    })
+  );
 
   // Event details
-  const contractAddress = "e223d8a629e49c68";
-  const contractName = "FUSD";
-  const eventName = "TokensWithdrawn";
-
-  // TODO: Uncomment
-  // if (fromBlock <= toBlock) {
-  if (1 < 5) {
-    try {
-      // Query for events
-      // TODO:Uncomment
-      // const result = await fcl.send([
-      //   fcl.getEventsAtBlockHeightRange(`A.${contractAddress}.${contractName}.${eventName}`, fromBlock, toBlock)
-      // ]);
-      // const decoded = await fcl.decode(result)
-      // console.log(`Events: ${JSON.stringify(decoded)}`)
-      // TODO: Update block cursor
-      // If we have some events, return them
-    } catch (error) {}
-  }
 };
+
+getEvents();
 
 module.exports.getListingCompletedEvents = async () => {
   // Get the most recent sealed block
@@ -120,6 +116,38 @@ module.exports.ListingsAvailableData = [
 module.exports.ListingsCompletedData = [
   {
     data: {
+      uuid: "",
+      address: "0xt987oghui",
+      dna: "6987tyughj8t7oyiuhl",
+      name: "guyog",
+    },
+  },
+  {
+    data: {
+      uuid: "2",
+      address: "0xt987oghui",
+      dna: "6987tyughj8t7oyiuhl",
+      name: "guyog",
+    },
+  },
+  {
+    data: {
+      uuid: "3",
+      address: "0xt987oghui",
+      dna: "6987tyughj8t7oyiuhl",
+      name: "guyog",
+    },
+  },
+  {
+    data: {
+      uuid: "1",
+      address: "0xt987oghui",
+      dna: "6987tyughj8t7oyiuhl",
+      name: "guyog",
+    },
+  },
+  {
+    data: {
       uuid: "4",
       address: "0xt987oghui",
       dna: "6987tyughj8t7oyiuhl",
@@ -136,15 +164,7 @@ module.exports.ListingsCompletedData = [
   },
   {
     data: {
-      uuid: "5",
-      address: "0xt987oghui",
-      dna: "6987tyughj8t7oyiuhl",
-      name: "guyog",
-    },
-  },
-  {
-    data: {
-      uuid: "5",
+      uuid: "6",
       address: "0xt987oghui",
       dna: "6987tyughj8t7oyiuhl",
       name: "guyog",
