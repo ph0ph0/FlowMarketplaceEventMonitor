6e03c9b8b2827296b317a331e27438e9c625bd79
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-10-06 08:59:39 +0100

    added some error handling to utils

--- FlowEventMonitorLambda/package/utils/index.js
+++ FlowEventMonitorLambda/package/utils/index.js
@@ -6,56 +6,68 @@ module.exports.batch = (arr, size) =>
 
 // Takes two object arrays and removes duplicates from within arrays, and across both arrays simultaneously, returning only unique elements across both arrays
 module.exports.deduplicateArrays = (arr1, arr2) => {
-  // Remove duplicates within arrays
-  const arr1Ids = arr1.map((e) => e.listingResourceID);
-  const arr1Filtered = arr1.filter(
-    (e, index) => !arr1Ids.includes(e.listingResourceID, index + 1)
-  );
-  const arr2Ids = arr2.map((e) => e.listingResourceID);
-  const arr2Filtered = arr2.filter(
-    (e, index) => !arr2Ids.includes(e.listingResourceID, index + 1)
-  );
+  try {
+    // Remove duplicates within arrays
+    const arr1Ids = arr1.map((e) => e.listingResourceID);
+    const arr1Filtered = arr1.filter(
+      (e, index) => !arr1Ids.includes(e.listingResourceID, index + 1)
+    );
+    const arr2Ids = arr2.map((e) => e.listingResourceID);
+    const arr2Filtered = arr2.filter(
+      (e, index) => !arr2Ids.includes(e.listingResourceID, index + 1)
+    );
 
-  // Remove duplicates across arrays
-  arr1Filtered.forEach((element1, i) => {
-    arr2Filtered.forEach((element2, j) => {
-      if (element1.listingResourceID == element2.listingResourceID) {
-        // Get index of element 1 in arr1, same for element2 and array2
-        delete arr1Filtered[i];
-        delete arr2Filtered[j];
-      }
+    // Remove duplicates across arrays
+    arr1Filtered.forEach((element1, i) => {
+      arr2Filtered.forEach((element2, j) => {
+        if (element1.listingResourceID == element2.listingResourceID) {
+          // Get index of element 1 in arr1, same for element2 and array2
+          delete arr1Filtered[i];
+          delete arr2Filtered[j];
+        }
+      });
     });
-  });
-  return [
-    arr1Filtered.filter((e) => e !== undefined),
-    arr2Filtered.filter((e) => e !== undefined),
-  ];
+    return [
+      arr1Filtered.filter((e) => e !== undefined),
+      arr2Filtered.filter((e) => e !== undefined),
+    ];
+  } catch (error) {
+    throw new Error(`Utils deduplicateArrays(): ${error}`);
+  }
 };
 
 module.exports.createdListingParser = (eventObject) => {
-  console.log(
-    `eventObject (created) to be parsed: ${JSON.stringify(eventObject)}`
-  );
-  const data = eventObject.data;
-  const event = {
-    storefrontAddress: data.storefrontAddress,
-    listingResourceID: data.listingResourceID,
-    dappyID: data.dappyID,
-    name: data.name,
-    dna: data.dna,
-    ftVaultType: data.ftVaultType,
-    price: data.price,
-  };
-  return event;
+  try {
+    console.log(
+      `eventObject (created) to be parsed: ${JSON.stringify(eventObject)}`
+    );
+    const data = eventObject.data;
+    const event = {
+      storefrontAddress: data.storefrontAddress,
+      listingResourceID: data.listingResourceID,
+      dappyID: data.dappyID,
+      name: data.name,
+      dna: data.dna,
+      ftVaultType: data.ftVaultType,
+      price: data.price,
+    };
+    return event;
+  } catch (error) {
+    throw new Error(`Utils createdListingParser(): ${error}`);
+  }
 };
 
 module.exports.completedListingParser = (eventObject) => {
-  console.log(
-    `eventObject (completed) to be parsed: ${JSON.stringify(eventObject)}`
-  );
-  const data = eventObject.data;
-  const event = {
-    listingResourceID: data.listingResourceID,
-  };
-  return event;
+  try {
+    console.log(
+      `eventObject (completed) to be parsed: ${JSON.stringify(eventObject)}`
+    );
+    const data = eventObject.data;
+    const event = {
+      listingResourceID: data.listingResourceID,
+    };
+    return event;
+  } catch (error) {
+    throw new Error(`Utils completedListingParser(): ${error}`);
+  }
 };
