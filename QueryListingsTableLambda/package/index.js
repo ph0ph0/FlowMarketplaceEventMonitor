d4ea254438c71d6dcb1f87102997ad3eb6037f92
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-10-07 13:49:07 +0100

    added more annotation and neated up

--- QueryListingsTableLambda/package/index.js
+++ QueryListingsTableLambda/package/index.js
@@ -2,6 +2,14 @@ const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient({ region: `us-east-1` });
 const TableName = process.env.VOTE_TABLE_NAME;
 
-const qltl = (event) => {};
+const qltl = (event) => {
+  console.log(`Event: ${event}`);
+
+  const tableName = "ListingTable";
+
+  const queryParams = {
+    TableName: tableName,
+  };
+};
 
 qltl();
